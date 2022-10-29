61ed8e30c107ef3422ce0113af153c9ee5fb9f0e
MartinLambrecht <69395872+MartinLambrecht@users.noreply.github.com>
2022-10-29 17:11:47 -0300

    Reportes Web

---
+++ UI.Web/Informes.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Informes.aspx.cs" Inherits="UI.Web.Informes" %>
+
+<%@ Register Assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" Namespace="CrystalDecisions.Web" TagPrefix="CR" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceholder" runat="server">
+    <br />
+    <div class="d-flex justify-content-around">
+        <asp:Button ID="btnUsuarios" runat="server" Text="Listado de Usuarios" OnClick="btnUsuarios_Click1" CssClass="btn btn-primary"/>
+        <asp:Button ID="btnCupoPorMateria" runat="server" Text="Cupo por Materias" OnClick="btnCupoPorMateria_Click" CssClass="btn btn-primary"/>
+    </div>
+    <CR:CrystalReportViewer ID="crv" runat="server" AutoDataBind="true" />
+    </asp:Content>
---
+++ UI.Web/Informes.aspx.cs
@@ -0,0 +1,52 @@
+﻿using Business.Logic;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Web;
+using Data.Database;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UI.Web.DSCupoPorMateriaTableAdapters;
+using static UI.Web.DSCupoPorMateria;
+
+namespace UI.Web
+{
+    public partial class Informes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnUsuarios_Click1(object sender, EventArgs e)
+        {
+            ReporteUsuarios rUsuarios = new ReporteUsuarios();
+            rUsuarios.SetDataSource(new UsuarioLogic().GetAll());
+            crv.ReportSource = rUsuarios;
+
+            rUsuarios.ExportToHttpResponse(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, Response, false, "Informacion Usuarios.");
+        }
+
+        protected void btnCupoPorMateria_Click(object sender, EventArgs e)
+        {
+            CupoPorMateriaTableAdapter da = new CupoPorMateriaTableAdapter();
+            DSCupoPorMateria dsCupoPorMateria = new DSCupoPorMateria();
+
+            CupoPorMateriaDataTable dt = (CupoPorMateriaDataTable)dsCupoPorMateria.Tables["CupoPorMateria"];
+
+            da.Fill(dt);
+
+            ReporteCupoPorMateria rCupoPorMateria = new ReporteCupoPorMateria();
+            rCupoPorMateria.SetDataSource(dsCupoPorMateria);
+            crv.ReportSource = rCupoPorMateria;
+
+            rCupoPorMateria.ExportToHttpResponse(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, Response, false, "Informacion Notas.");
+
+        }
+    }
+}
